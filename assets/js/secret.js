642c6cee484921fb72b2dafa30f6300b2be5a6ae
TEDA <mohanasudan63@gmail.com>
2026-08-28 22:27:50 -0400

    Fix

--- assets/js/secret.js
+++ assets/js/secret.js
@@ -4,17 +4,15 @@ let secretIndex = 0;
 document.addEventListener("keydown", function (event) {
     const key = event.key.toLowerCase();
 
-    if (tedaAccess !== "true") {
-        window.location.href = "/News/index.html";
-    }
-
     if (key === secretCode[secretIndex]) {
         secretIndex++;
+
         if (secretIndex === secretCode.length) {
             sessionStorage.setItem("tedaAccess", "true");
-            window.location.href = "/index.html";
             secretIndex = 0;
+            window.location.href = "/index.html";
         }
+
     } else {
         secretIndex = key === "t" ? 1 : 0;
     }
